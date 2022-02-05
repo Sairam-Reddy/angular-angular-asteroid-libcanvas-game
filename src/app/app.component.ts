ae66c28608edae3c13c1ed59f75007d6b371ebd8
Sairam-Reddy <sairamreddyg33@gmail.com>
2022-02-05 11:54:14 +0530

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -223,40 +223,7 @@ export class AppComponent {
 
    
 
-    /** @class Ast.Explosion */
-    declare('Ast.Explosion', App.Element, {
-      zIndex: 5,
-      angle: 0,
-
-      configure: function () {
-        this.settings.get('controller').sounds.play('explosion');
-
-        this.angle = Number.random(0, 360).degree();
-        this.animation = new Animation({
-          sheet: this.settings.get('sheet'),
-          onUpdate: this.redraw,
-          onStop: this.destroy,
-        });
-
-        for (var i = Number.random(5, 8); i--; ) {
-          new Ast.Explosion.Debris(this.layer, {
-            shape: new Circle(this.shape.center.clone(), 5),
-          });
-        }
-      },
-
-      renderTo: function (ctx) {
-        var image = this.animation.get();
-
-        if (image) {
-          ctx.drawImage({
-            image: image,
-            center: this.shape.center,
-            angle: this.angle,
-          });
-        }
-      },
-    });
+   
 
     /** @name Ast.Explosion.Debris */
     atom.declare('Ast.Explosion.Debris', App.Element, {
---
+++ src/app/models/explosion.model.ts
@@ -0,0 +1,37 @@
+/** @class Ast.Explosion */
+
+declare var atom: any;
+
+atom.declare('Ast.Explosion', App.Element, {
+  zIndex: 5,
+  angle: 0,
+
+  configure: function () {
+    this.settings.get('controller').sounds.play('explosion');
+
+    this.angle = Number.random(0, 360).degree();
+    this.animation = new Animation({
+      sheet: this.settings.get('sheet'),
+      onUpdate: this.redraw,
+      onStop: this.destroy,
+    });
+
+    for (var i = Number.random(5, 8); i--; ) {
+      new Ast.Explosion.Debris(this.layer, {
+        shape: new Circle(this.shape.center.clone(), 5),
+      });
+    }
+  },
+
+  renderTo: function (ctx) {
+    var image = this.animation.get();
+
+    if (image) {
+      ctx.drawImage({
+        image: image,
+        center: this.shape.center,
+        angle: this.angle,
+      });
+    }
+  },
+});
