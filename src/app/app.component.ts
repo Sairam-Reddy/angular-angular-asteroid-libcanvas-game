63f0fa9f72577b8d409f82ed92e602f98a2e1981
Sairam-Reddy <sairamreddyg33@gmail.com>
2022-02-05 11:58:36 +0530

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -59,14 +59,6 @@ export class AppComponent {
       new Ast.Controller();
     });
 
-    
-
-   
-
-   
-
-    
-
     /* Stones */
 
     /** @class Ast.Stones.Set */
@@ -161,10 +153,6 @@ export class AppComponent {
     });
     /* Stones */
 
-   
-
-   
-
     /** @name Ast.Explosion.Debris */
     atom.declare('Ast.Explosion.Debris', App.Element, {
       zIndex: 7,
@@ -194,65 +182,5 @@ export class AppComponent {
         });
       },
     });
-    
-
-   
-
-  
-
-   
-
-    /** @class Ast.Sounds */
-    declare('Ast.Sounds', {
-      ext: null,
-      map: null,
-      sounds: null,
-      prefix: '//libcanvas.github.io/games/asteroids/',
-
-      initialize: function (prefix, map) {
-        var elem = document.createElement('audio'),
-          cpt = elem.canPlayType;
-        // some bugs with audio in firefox
-        if (cpt && navigator.userAgent.toLowerCase().indexOf('firefox') == -1) {
-          if (cpt.call(elem, 'audio/ogg; codecs="vorbis"')) this.ext = 'ogg';
-          else if (cpt.call(elem, 'audio/mpeg;')) this.ext = 'mp3';
-        }
-
-        this.prefix = prefix;
-        this.map = map || {};
-        this.sounds = {};
-      },
-
-      getFileName: function (name) {
-        return this.prefix + (this.map[name] || name) + '.' + this.ext;
-      },
-
-      play: function (name) {
-        if (!this.ext) return;
-
-        var audio,
-          sounds = this.sounds,
-          i,
-          max = null;
-
-        if (!sounds[name]) sounds[name] = [];
-
-        for (i = sounds[name].length; i--; ) {
-          audio = sounds[name][i];
-          if (audio.ended) break;
-          audio = null;
-        }
-
-        if (!audio) {
-          audio = document.createElement('audio');
-          audio.src = this.getFileName(name);
-          sounds[name].push(audio);
-        }
-
-        audio.volume = 1;
-        audio.play();
-      },
-    });
-    /* Sounds */
   }
 }
---
+++ src/app/models/sounds.model.ts
@@ -0,0 +1,54 @@
+/** @class Ast.Sounds */
+declare var atom: any;
+
+atom.declare('Ast.Sounds', {
+  ext: null,
+  map: null,
+  sounds: null,
+  prefix: '//libcanvas.github.io/games/asteroids/',
+
+  initialize: function (prefix, map) {
+    var elem = document.createElement('audio'),
+      cpt = elem.canPlayType;
+    // some bugs with audio in firefox
+    if (cpt && navigator.userAgent.toLowerCase().indexOf('firefox') == -1) {
+      if (cpt.call(elem, 'audio/ogg; codecs="vorbis"')) this.ext = 'ogg';
+      else if (cpt.call(elem, 'audio/mpeg;')) this.ext = 'mp3';
+    }
+
+    this.prefix = prefix;
+    this.map = map || {};
+    this.sounds = {};
+  },
+
+  getFileName: function (name) {
+    return this.prefix + (this.map[name] || name) + '.' + this.ext;
+  },
+
+  play: function (name) {
+    if (!this.ext) return;
+
+    var audio,
+      sounds = this.sounds,
+      i,
+      max = null;
+
+    if (!sounds[name]) sounds[name] = [];
+
+    for (i = sounds[name].length; i--; ) {
+      audio = sounds[name][i];
+      if (audio.ended) break;
+      audio = null;
+    }
+
+    if (!audio) {
+      audio = document.createElement('audio');
+      audio.src = this.getFileName(name);
+      sounds[name].push(audio);
+    }
+
+    audio.volume = 1;
+    audio.play();
+  },
+});
+/* Sounds */
