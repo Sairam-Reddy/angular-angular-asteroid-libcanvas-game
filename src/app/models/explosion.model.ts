53b7c75427383c536eaf604db9155338a76a4285
Sairam-Reddy <sairamreddyg33@gmail.com>
2022-02-05 12:06:15 +0530

    1 file modified

--- src/app/models/explosion.model.ts
+++ src/app/models/explosion.model.ts
@@ -36,3 +36,33 @@ atom.declare('Ast.Explosion', App.Element, {
     }
   },
 });
+
+/** @name Ast.Explosion.Debris */
+atom.declare('Ast.Explosion.Debris', App.Element, {
+  zIndex: 7,
+  angle: 0,
+
+  configure: function () {
+    this.angle = Number.random(1, 360).degree();
+
+    new atom.Animatable(this).animate({
+      time: 1500,
+      props: {
+        'shape.center.x': this.shape.center.x + Number.random(-200, 200),
+        'shape.center.y': this.shape.center.y + Number.random(-200, 200),
+        opacity: 0,
+      },
+      fn: 'circ-out',
+      onTick: this.redraw,
+      onComplete: this.destroy,
+    });
+  },
+
+  renderTo: function (ctx, resources) {
+    ctx.drawImage({
+      image: resources.get('images').get('debris'),
+      center: this.shape.center,
+      angle: this.angle,
+    });
+  },
+});
